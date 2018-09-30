e0912abc5f4018210e7e626861bf28f775fab353
davichiar <ardabitchy02@naver.com>
2018-10-01 02:52:51 +0900

    test

--- Face-Detect/main.py
+++ Face-Detect/main.py
@@ -1,24 +1,29 @@
-import requests
 import json
-import picture_class
-import picture_download
-import picture_detect
-import picture_search
+import requests
+from picture_class import picture_data
+from picture_download import getDownload
+from picture_search import faceSearch
+from picture_detect import faceDetect
 
-# url get
-try:
-    url = 'https://fc3i3hiwel.execute-api.ap-northeast-2.amazonaws.com/develop/pictures/processing'
-    response = requests.get(url)
-except:
-    print("URL 주소가 잘못되었습니다")
+# https://docs.google.com/document/d/1lwofKqyqlq--8LuiqjpFgP4R-7mPNt0JT5QwAcu_MRA/edit
+# FaceRecognition <-> Server
+
+# https://kidsharu.github.io/KidsHaru-APIDoc/
+# 자세한 API 문서
+
+url = "https://fc3i3hiwel.execute-api.ap-northeast-2.amazonaws.com/develop/pictures/processing"
 
 # data get
 try:
-    data = picture_class.picture_data()
+    # URL에서 정보 얻기
+    response = requests.get(url)
+
+    # 데이터 추가
+    data = picture_data()
     data.append_data(response)
 except:
-    print("사이트를 읽는데 실패하였습니다")
-    
+    print("URL 주소 인식 실패")
+
 # picture download
 try:
     for i in range(0, data.getLen()):
@@ -26,15 +31,14 @@ try:
         picture_id = str(data.getPictureId(i)).strip()
         status = data.getStatus(i)
         picture_url = str(data.getPictureUrl(i)).strip()
-
         # print(album_id, picture_id, status, picture_url)
-        download = picture_download.getDownload(album_id, picture_id, status, picture_url)
+        download = getDownload(album_id, picture_id, status, picture_url)
 except:
     print("사진을 저장하는데 실패하였습니다.")
 
 # picture detecting
 try:
-    for i in range(0, data.getLen()):
+    for i in range(0, 5):
         print(i)
         album_id = str(data.getAlbumId(i)).strip()
         picture_id = str(data.getPictureId(i)).strip()
@@ -42,10 +46,12 @@ try:
         picture_url = str(data.getPictureUrl(i)).strip()
 
         print(album_id, picture_id, status, picture_url)
-        img_before, img_after = picture_search.faceSearch(album_id, picture_id, status, picture_url)
-        if(img_before != -1 and img_after != -1):
-             checking = picture_detect.faceDetect(album_id, picture_id, status, picture_url, img_before, img_after)
+        img_before, img_after = faceSearch(album_id, picture_id, status, picture_url)
 
+        if(img_before != -1 and img_after != -1):
+             checking = faceDetect(album_id, picture_id, status, picture_url, img_before, img_after)
+
+        '''
         checking = 1
         if(checking == 1):
             put_url = "https://fc3i3hiwel.execute-api.ap-northeast-2.amazonaws.com/develop/albums/" + str(album_id) + "/pictures/" + str(picture_id)
@@ -53,7 +59,6 @@ try:
         json_data = {
              "status" : 'processing'
         }
-            
         json_string = json.dumps(json_data).encode("utf-8")
         response = requests.put(put_url, data=json_string)
 
@@ -61,7 +66,7 @@ try:
            "status" : "checking"
         } 
         json_string = json.dumps(json_data).encode("utf-8")
-        response = requests.put(put_url, data=json_string) 
-
+        response = requests.put(put_url, data=json_string)
+        '''
 except:
     print("사진을 처리하는데 실패하였습니다.")
--- Face-Detect/picture_download.py
+++ Face-Detect/picture_download.py
@@ -15,18 +15,18 @@ def getDownload(album_id, picture_id, status, picture_url):
         os.mkdir("./" + dirname + "/")             # 없으면 생성하라
 
     dirname += "/" + picture_id + ".jpg"
-    print(dirname)
+    # print(dirname)
 
     if os.path.isfile("./" + dirname):
-        print("파일이 이미 있습니다")
+        # print("파일이 이미 있습니다")
         return -1
 
     urllib.request.urlretrieve(picture_url, dirname)
     try:
         urllib.request.urlretrieve(picture_url, dirname)
-        print("다운로드 완료!")
+        # print("다운로드 완료!")
         return 1
     except:
-        print("다운로드 오류!")
+        # print("다운로드 오류!")
         return -2
     
