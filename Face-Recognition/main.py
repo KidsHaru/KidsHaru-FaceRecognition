b6cd3c215dd90da0ba6022407d1e7849af69f286
davichiar <ardabitchy02@naver.com>
2018-10-31 14:04:03 +0900

    test

--- Face-Recognition/main.py
+++ Face-Recognition/main.py
@@ -1,5 +1,7 @@
 import sys
 
+
+'''
 if __name__ == "__main__":
     if len(sys.argv) - 1:
         if(sys.argv[1] == '-d' or sys.argv[1] == '--download'):
@@ -9,3 +11,4 @@ if __name__ == "__main__":
 
     else:
         print('명령 인자를 바르게 입력해주세요')
+'''
